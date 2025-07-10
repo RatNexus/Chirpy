4b1cbe111484d52b476871a9a7b80d262543c0db
RatNexus <michal.szalachowski.33@gmail.com>
2025-07-10 10:40:48 +0200

    feat: add file server and sample index.html

--- main.go
+++ main.go
@@ -7,21 +7,23 @@ import (
 )
 
 func exitWithError(err error) {
+	fmt.Println("Server exited with an error.")
 	fmt.Println(err)
 	os.Exit(1)
 }
 
 func main() {
 	serveMux := http.NewServeMux()
+	serveMux.Handle("/", http.FileServer(http.Dir("./static/")))
+
 	server := &http.Server{
 		Addr:    ":8080",
 		Handler: serveMux,
 	}
 
-	fmt.Println("Start server.")
+	fmt.Println("Start server on http://localhost:8080/")
 	err := server.ListenAndServe()
 	if err != nil {
-		fmt.Println("Server exited with an error.")
 		exitWithError(err)
 	}
 	fmt.Println("Server exited with no errors.")
