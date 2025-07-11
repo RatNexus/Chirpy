228f527cb97e4eb28c7650f4d1801e99fecd5bc5
RatNexus <michal.szalachowski.33@gmail.com>
2025-07-11 10:38:44 +0200

    feat: update /admin/metrics to use html

--- main.go
+++ main.go
@@ -40,9 +40,16 @@ func main() {
 	}
 
 	hitsHandler := func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+		w.Header().Set("Content-Type", "text/html; charset=utf-8")
 		w.WriteHeader(200)
-		w.Write([]byte(fmt.Sprintf("Hits: %v", apiCfg.fileserverHits.Load())))
+		template := `<html>
+	<body>
+		<h1>Welcome, Chirpy Admin</h1>
+		<p>Chirpy has been visited %d times!</p>
+	</body>
+</html>`
+
+		w.Write([]byte(fmt.Sprintf(template, apiCfg.fileserverHits.Load())))
 	}
 
 	resetHandler := func(w http.ResponseWriter, r *http.Request) {
