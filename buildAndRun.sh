df9ad31117891a89b465e732ebd449e27099fb7a
RatNexus <michal.szalachowski.33@gmail.com>
2025-07-17 14:48:52 +0200

    build: improve build script

--- buildAndRun.sh
+++ buildAndRun.sh
@@ -1,4 +1,25 @@
 #!/bin/bash
 
-# Builds the server and runs it
-go build -o out && ./out
+output_path="."
+output_name="out"
+
+joined="${output_path}/${output_name}"
+
+# Run Unit tests. If they pass. -> Build the server. If succesfull. -> Run the server. 
+echo -e "----- Runing unit test -----" &&
+
+test_output=$(go test ./... 2>&1)
+test_status=$?
+if [ $test_status -ne 0 ]; then
+  echo -e "Some tests failed.\n"
+  echo "$test_output"
+  echo ""
+  exit 1
+else
+  echo "All is well."
+fi
+
+echo -e "\n----- Building into ${joined} -----" &&
+go build -o $joined &&
+echo -e "\n----- Running ${joined} -----\n" &&
+$joined
